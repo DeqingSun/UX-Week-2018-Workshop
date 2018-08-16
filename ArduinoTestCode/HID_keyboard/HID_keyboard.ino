826de6a9f7e33ad17e2117e20a80eab6f4a36cd7
Deqing Sun <iamseer@gmail.com>
2018-08-16 16:24:06 -0400

    Add WDT
    https://github.com/sandeepmistry/arduino-nRF5/pull/55/commits/725882185e2678145863369e9b5a2d93df30636e

--- ArduinoTestCode/HID_keyboard/HID_keyboard.ino
+++ ArduinoTestCode/HID_keyboard/HID_keyboard.ino
@@ -107,6 +107,12 @@ void setup() {
   accel.begin(false, 2); // 8-bit mode, 2g range
 #endif
 
+  //enable WDT, i2c hang occasionally
+  NRF_WDT->CONFIG = NRF_WDT->CONFIG = (WDT_CONFIG_HALT_Pause << WDT_CONFIG_HALT_Pos) | ( WDT_CONFIG_SLEEP_Run << WDT_CONFIG_SLEEP_Pos);
+  NRF_WDT->CRV = (32768 * 2000) / 1000;
+  NRF_WDT->RREN |= WDT_RREN_RR0_Msk;
+  NRF_WDT->TASKS_START = 1;
+
 }
 
 
@@ -119,6 +125,7 @@ void showPasskey(BLECentral& central) {
 
 
 void loop() {
+  NRF_WDT->RR[0] = WDT_RR_RR_Reload; //reset WDT
 
   static unsigned long previousSerialMillis = 0;
 
@@ -176,6 +183,7 @@ void loop() {
 #endif
 
     while (central.connected()) {
+      NRF_WDT->RR[0] = WDT_RR_RR_Reload; //reset WDT
 
       for (int i = 0; i < 5; i++) {
 #ifdef PIN2_OUTPUT
